bbb510c20a564d4ee89b0099ce74a69019590a6c
Zlatov Cristina <cristinazltv.dev@gmail.com>
2025-02-13 18:21:48 +0100

    Added Routing to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Hero from "./pages/Hero/Hero";
 import SkillsShowcase from "./pages/SkillsShowcase/SkillsShowcase";
 
 function App() {
   return (
-    <>
-      <Hero />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/skills" element={<SkillsShowcase />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
